3eba74154958566ce112693052e723bc6421f93a
Horacio Fernandez <horacio.fernandez@rackspace.com>
2014-08-11 10:39:01 -0500

    Init

---
+++ bootstrap.cmd
@@ -0,0 +1,3 @@
+﻿ren secrets secrets.ps1 &
+Powershell "Set-ExecutionPolicy Bypass;. C:\cloud-automation\secrets.ps1;NI -Path $d.wD -ItemType container;(New-Object System.Net.webclient).DownloadFile($($d.gS),$($d.gPath));Start -Wait $($d.gPath) -ArgumentList '/verysilent';chdir -Path $d.wD;Start -Wait $($d.gX) -ArgumentList \"clone https://$($d.gAPI + \"@github.com/\" + $d.gU + \"/\" + $d.mR + \".git\")\";chdir -Path $($d.wD + '\' + $d.mR);Start -Wait $($d.gX) -ArgumentList \"checkout $($d.br)\";Powershell $($d.wD, $d.mR, $d.bS -join '\')"
+
