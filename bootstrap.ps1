c59c386f56eb5e64bd1a8af1ed3ce9f88cce8488
Mark Key <mark.key@rackspace.com>
2014-10-30 07:26:11 -0500

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,10 +3,10 @@ if(!(Test-Path -Path "C:\DevOps")) {
    New-Item -Path "C:\DevOps" -ItemType directory -Force
 }
 . "C:\DevOps\secrets.ps1"
-(New-Object System.Net.webclient).DownloadFile(($d.gS),($d.gPath))
-Start -Wait $($d.gPath) -ArgumentList "/verysilent"
-cd $($d.mod)
-Start -Wait $($d.gX) -ArgumentList "clone --branch $($d.cBr) $(($d.git), $($d.gCA), 'rsCommon.git' -join '/')"
-cd $($d.wD)
-Start -Wait $($d.gX) -ArgumentList "clone --branch $($d.provBr) $((($d.git), $($d.gCA), $($d.prov) -join '/'), '.git' -join '')"
-powershell.exe "$($d.wD, $d.prov, $d.bS -join '\')"
+(New-Object System.Net.webclient).DownloadFile("https://raw.githubusercontent.com/rsWinAutomationSupport/Git/v1.9.4/Git-Windows-Latest.exe","C:\DevOps\Git-Windows-Latest.exe")
+Start -Wait "C:\DevOps\Git-Windows-Latest.exe" -ArgumentList "/verysilent"
+cd "C:\Program Files\WindowsPowerShell\Modules"
+Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "clone --branch $($d.cBr) $('https://github.com', $($d.gCA), 'rsCommon.git' -join '/')"
+cd "C:\DevOps"
+Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "clone --branch $($d.provBr) $('https://github.com', $($d.gCA), 'rsProvisioning.git' -join '/')"
+& "C:\DevOps\rsProvisioning\rsBasePrep.ps1"
