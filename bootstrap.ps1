7ff7793c882b6c62d98b57f5b39a0ad5b7b3c8d8
Mark Key <mark.key@rackspace.com>
2014-11-01 12:12:19 -0500

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,7 +3,11 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
 (New-Object System.Net.webclient).DownloadFile("https://raw.githubusercontent.com/rsWinAutomationSupport/Git/v1.9.4/Git-Windows-Latest.exe","C:\DevOps\Git-Windows-Latest.exe")
 Start -Wait "C:\DevOps\Git-Windows-Latest.exe" -ArgumentList "/verysilent"
 cd "C:\Program Files\WindowsPowerShell\Modules"
+### the rsCommon production command is commented out for dev use.
+#Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "clone --branch $($d.provBr) https://github.com/rsWinAutomationSupport/rsCommon.git"
 Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "clone --branch $($d.provBr) $('https://github.com', $($d.git_username), 'rsCommon.git' -join '/')"
 cd "C:\DevOps"
+### the rsProvisioning production command is commented out for dev use.
+#Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "clone --branch $($d.provBr) https://github.com/rsWinAutomationSupport/rsProvisioning.git"
 Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "clone --branch $($d.provBr) $('https://github.com', $($d.git_username), 'rsProvisioning.git' -join '/')"
-PowerShell.exe "C:\DevOps\rsProvisioning\rsBasePrep.ps1"
+PowerShell.exe "C:\DevOps\rsProvisioning\rsBasePrep.ps1" -ArgumentList "-ExecutionPolicy Bypass -Force"
